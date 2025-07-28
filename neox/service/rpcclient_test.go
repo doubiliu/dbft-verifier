9354ef3576c87c4aeb44a732eb7c79a37b2fa45b
doubiliu <lmy0930@126.com>
2025-07-28 10:44:18 +0800

    update neox rpc invoke

--- neox/service/rpcclient_test.go
+++ neox/service/rpcclient_test.go
@@ -1,29 +1,26 @@
 package service
 
 import (
-	"context"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"math/big"
 	"testing"
+	"time"
 )
 
 func Test_rpc(t *testing.T) {
-	client, err := ethclient.Dial("https://neoxt4seed1.ngd.network/")
-	if err != nil {
-		t.Fatal(err)
-	}
-	number, err := client.BlockNumber(context.Background())
+	xClient, err := NewNeoXClient("https://neoxt4seed1.ngd.network/")
 	if err != nil {
 		panic(err)
 	}
-	println(number)
-	header, err := client.HeaderByNumber(context.Background(), big.NewInt(int64(number)))
-	if err != nil {
-		panic(err)
+	for i := 0; i < 10; i++ {
+		blockHeader, err := xClient.GetCurrentBlockHeader()
+		if err != nil {
+			panic(err)
+		}
+		data, err := blockHeader.MarshalJSON()
+		if err != nil {
+			return
+		}
+		println(string(data))
+		time.Sleep(5 * time.Second)
 	}
-	data, err := header.MarshalJSON()
-	if err != nil {
-		return
-	}
-	println(string(data))
+	defer xClient.Close()
 }
